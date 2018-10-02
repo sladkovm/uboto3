74217d5c44e5754be117c23a27e16b1325594d24
Maksym Sladkov <sladkovm@gmail.com>
2018-10-02 15:24:33 +0200

    setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/sladkovm/uboto3",
     packages=['uboto3'],
-    install_requires=['boto3']
+    install_requires=['boto3'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
